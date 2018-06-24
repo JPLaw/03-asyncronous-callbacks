8c501baa4e42bcf751ac8b6206a8a6ba8e88c535
Jennifer <jlawrence010@gmail.com>
2018-06-24 11:03:09 -0700

    trying new tests

--- src/__test__/file-reader.test.js
+++ src/__test__/file-reader.test.js
@@ -1,42 +1,51 @@
 'use strict';
 
 const fs = require('fs');
-const fileReader = require('./lib/reader/');
+const reader = require('./../lib/reader');
 
-describe('testing to see if I am connected to TravisCI', () => {
-  test('should equal true', () => {
-    expect(true).toEqual(true);
-  });
-});
-  
-const mockData1 = `${__dirname}/./mock-assets/mock-data1.txt`;
-const mockData2 = `${__dirname}/./mock-assets/mock-data2.txt`;
-const mockData3 = `${__dirname}/./mock-assets/mock-data3.txt`;
+const mockText1 = `${__dirname}/./mock-assets/1.txt`;
+const mockText2 = `${__dirname}/./mock-assets/2.txt`;
+const mockText3 = `${__dirname}/./mock-assets/3.txt`;
 
-let mockData = [];
-
-describe('testing fileReader module that reads given files', () => {
+describe('#READER: file reader module that reads exactly three files', () => {
+  let mockData;
   beforeAll(() => {
     mockData = [
-      fs.readFileSync(mockData1, { encoding: 'utf-8' }),
-      fs.readFileSync(mockData2, { encoding: 'utf-8' }),
-      fs.readFileSync(mockData3, { encoding: 'utf-8' }), 
+      fs.readFile(mockText1, { encoding: 'utf-8' }),
+      fs.readFile(mockText2, { encoding: 'utf-8' }),
+      fs.readFile(mockText3, { encoding: 'utf-8' }),
     ];
   });
 
-  it('should show that the data in our readAllFiles equals the mockData array', () => {
-    fileReader.readThreeFiles(mockData1, mockData2, mockData3, (err, data1, data2, data3) => {
-      expect(data1).toEqual(mockData1[0]);
-      expect(err).toBeNull();
-      expect(data2).toEqual(mockData2[1]);
-      expect(err).toBeNull();
-      expect(data3).toEqual(mockData3[2]);
+  it('returns the mock data in the correct order', () => {
+    const testPaths = [mockText1, mockText2, mockText3];
+    reader(testPaths, (err, data) => {
+      expect(data[0]).toEqual(mockData[0]);
+      expect(data[1]).toEqual(mockData[1]);
+      expect(data[2]).toEqual(mockData[2]);
       expect(err).toBeNull();
     });
   });
 
-  it('should return an error for bad file path on first item', () => {
-    fileReader.readThreeFiles('bad path', mockData2, mockData3, (err, data1, data2, data3) => {
+  it('throws an error on bad path at position 1', () => {
+    const testPaths = ['bad path', mockText2, mockText3];
+    reader(testPaths, (err) => {
+      expect(err).toHaveProperty('errno');
+      expect(err.code).toEqual('ENOENT');
+    });
+  });
+
+  it('throws an error on bad path at position 2', () => {
+    const testPaths = [mockText1, 'bad path', mockText3];
+    reader(testPaths, (err) => {
+      expect(err).toHaveProperty('errno');
+      expect(err.code).toEqual('ENOENT');
+    });
+  });
+
+  it('throws an error on bad path at position 3', () => {
+    const testPaths = [mockText1, mockText2, 'bad path'];
+    reader(testPaths, (err) => {
       expect(err).toHaveProperty('errno');
       expect(err.code).toEqual('ENOENT');
     });
